6b5ec52e19a83cbe16a65df27c6547c04ccead71
Victor Gabriel de Aquino <victorgabdev@gmail.com>
2026-04-18 17:40:01 -0300

    first commit

---
+++ src/main/java/com/victorgabdev/api_genciamento_clima_cidade/ApiGenciamentoClimaCidadeApplication.java
@@ -0,0 +1,13 @@
+package com.victorgabdev.api_genciamento_clima_cidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGenciamentoClimaCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGenciamentoClimaCidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorgabdev/api_genciamento_clima_cidade/ApiGenciamentoClimaCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgabdev.api_genciamento_clima_cidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGenciamentoClimaCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
